4f92d11da14c48c72d3fcf42bf4674215fd7830a
Shashwat Singh <shashwat0045@gmail.com>
2025-08-29 00:31:15 +0530

    fixed root directory

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div>
-      Shashwat Singh
-    </div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/dashboard"); 
+  }, [router]);
+
+  return null;
 }
