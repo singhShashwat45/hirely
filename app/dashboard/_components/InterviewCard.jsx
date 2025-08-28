3782012d8f66b43bee56e7f715379666db84759e
Shashwat Singh <shashwat0045@gmail.com>
2025-08-28 10:05:51 +0530

    Added Timestamp in interview card

--- app/dashboard/_components/InterviewCard.jsx
+++ app/dashboard/_components/InterviewCard.jsx
@@ -11,12 +11,30 @@ function InterviewCard({interview}) {
     const onFeedback = ()=>{
         router.push("dashboard/interview/"+interview?.mockId+"/feedback");
     }
+    let formattedDate = "N/A";
+    if (interview?.createdAt) {
+        try {
+        const [day, month, year] = interview.createdAt.split("-"); // "28-08-2025"
+        const dateObj = new Date(`${year}-${month}-${day}T${new Date().toLocaleTimeString("en-US", { hour12: false })}`); 
+        formattedDate = dateObj.toLocaleString("en-US", {
+            day: "2-digit",
+            month: "short",
+            year: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: true,
+        });
+        } catch (err) {
+        console.error("Date parse error:", err);
+        }
+    }
+
     return (
     <div className="border shadow-sm rounded-lg p-3">
         <h2 className="font-bold text-fuchsia-500">{interview?.jobPosition}</h2>
         <h2 className="text-sm text-gray-700">Years of Experience: {interview?.jobExperience}</h2>
         <h2 className="text-xs text-gray-500">
-            Taken On: {interview?.createdAt}</h2>
+            Taken On: {formattedDate}</h2>
         
         <div className="flex justify-end gap-2">
             <Button size="sm" variant="outline" 
